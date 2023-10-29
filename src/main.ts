cd92592e7104c4225ca01931703b314e279563ef
azotov <zotovprog@gmail.com>
2023-10-29 23:19:55 +0300

    feat: update conf

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.APP_PORT ? Number(process.env.APP_PORT) : 3000);
 }
 bootstrap();
